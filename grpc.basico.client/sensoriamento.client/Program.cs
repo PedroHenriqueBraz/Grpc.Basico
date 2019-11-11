64bb43a3bcb180437ad18b84da78a2213a318923
Pedro H Braz <braz.hpedro@gmail.com>
2019-11-10 22:15:11 -0300

    Added new server and client solution

---
+++ grpc.basico.client/sensoriamento.client/Program.cs
@@ -0,0 +1,21 @@
+﻿using grpc.basico.server;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace sensoriamento.client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var sensorClient = new Sensor.SensorClient(channel);
+
+            var sensorData = await sensorClient.GetLastSensorDataAsync(new SensorRequest() { SensorId = 1});
+
+            Console.WriteLine($"x: {sensorData.PositionX} \n y: {sensorData.PositionY} \n temperatura: {sensorData.Temperature}");
+        }
+    }
+}
---
+++ grpc.basico.server/grpc.basico.server/Datas/Dados.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sensoriamento.server.Datas
+{
+    public static class Dados
+    {
+        public static List<Sensor> sensors => gerarsensores();
+
+        private static List<Sensor> gerarsensores()
+        {
+            List<Sensor> sensores = new List<Sensor>();
+            var rand = new Random();
+
+            for(int i =0; i < 100; i++)
+            {
+                List<double> temps = new List<double>();
+
+                for(int j = 0; j < 50; j++)
+                {
+                    var temp = rand.Next(10, 30) + rand.NextDouble();
+                    temps.Add(temp);
+                }
+                sensores.Add(new Sensor(i, rand.NextDouble(), rand.NextDouble(), temps));
+            }
+            return sensores;
+        }
+    }
+
+    public class Sensor
+    {
+        public Sensor(int id, double x, double y, List<double> temps)
+        {
+            this.sensor_id = id;
+            this.position_x = x;
+            this.position_y = y;
+            this.temperatures = temps;
+        }
+        public int sensor_id { get; set; }
+        public double position_x { get; set; }
+        public double position_y { get; set; }
+        public List<double> temperatures { get; set; }
+    }
+}
