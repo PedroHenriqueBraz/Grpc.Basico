582b9d9b7f0cc94d3993a815515fc4a1003b8d19
Pedro H Braz <braz.hpedro@gmail.com>
2019-11-23 22:22:34 -0300

    add new methods

--- grpc.basico.server/grpc.basico.server/Controllers/SensorController.cs
+++ grpc.basico.server/grpc.basico.server/Controllers/SensorController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 using sensoriamento.server.Sensores;
@@ -30,5 +31,60 @@ namespace grpc.basico.server
             return Task.FromResult(sensor);
         }
 
+        public override async Task GetAllData(Empty request, IServerStreamWriter<SensorResponse> responseStream, 
+            ServerCallContext context)
+        {
+            foreach (var sensor in Sensores.sensores)
+            {
+                await Task.Delay(5000);
+                await responseStream.WriteAsync(new SensorResponse()
+                {
+                    Sensor = sensor
+                });
+            }
+        }
+
+        public override async Task<AddPhotoResponse> AddPhoto(IAsyncStreamReader<AddPhotoRequest> requestStream, 
+            ServerCallContext context)
+        {
+            var data = new List<byte>();
+            
+            while (await requestStream.MoveNext())
+            {
+                Console.WriteLine("Received " +
+                    requestStream.Current.Data.Length + " bytes");
+                data.AddRange(requestStream.Current.Data);
+            }
+            Console.WriteLine("Received file with " + data.Count + " bytes");
+
+            return new AddPhotoResponse()
+            {
+                IsOk = true
+            };
+        }
+
+        public override async Task AddNewSensor(IAsyncStreamReader<SensorAddRequest> requestStream, 
+            IServerStreamWriter<SensorResponse> responseStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext())
+            {
+                var newSensor = requestStream.Current.Sensor;
+
+                lock (this)
+                {
+                    Sensores.sensores.Add(newSensor);
+                }
+
+                await responseStream.WriteAsync(new SensorResponse()
+                {
+                    Sensor = newSensor
+                });
+            }
+
+            foreach (var sensor in Sensores.sensores)
+            {
+                Console.WriteLine(sensor);
+            }
+        }
     }
 }
